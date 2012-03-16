b1380fa362e96fea12f5e6e863c27544f86b5502
GerbenGaastra <gaastra@gtga.nl>
2012-03-16 10:44:10 +0100

    Added a bit performance, variable outFile, and added error checking to fastaRewrite

--- Build-Package/BLASTnParse/R/fastaRewrite.R
+++ Build-Package/BLASTnParse/R/fastaRewrite.R
@@ -7,19 +7,16 @@
 ## a vector which can be written to file as a valid (ncbi) fasta file
 ## input collumn 1 must contain the fasta ID
 ## input collumn 2 must contain the sequence
-fastaRewrite<- function(input) {
-  #if( class(input) != "matrix") { stop("'input' is not an matrix") }
+fastaRewrite<- function(input,outFile) {
+  if( missing(input) ) { stop("argument 'input' is missing") }
+  if( missing(outFile) ) { stop("argument 'outFile' is missing") }
   if( ncol(input) != 2 ) { stop("'input' has does not have 2 collumns") }
+  if( mode(outFile) != "character" ) { stop("mode 'outFile' is not a char string") }
   output <- NULL
-  fp <- file("output.fasta","w")
-  for( i in 1:nrow(input) ) {
-    #if( (i %% 1000) == 0 ) {
-    #  cat("parsing row",i,"\n",sep=" ")
-    #  write(output,"input.fasta",append=TRUE,sep="\t")
-    #  output <- NULL
-    #}
-    writeLines(as.character(input[i,1]), con=fp)
-    writeLines(as.character(input[i,2]), con=fp)
-  }
+  fp <- file(outFile,"w")
+  apply( input,1,function(x) {
+    writeLines(as.character(x[1]), con=fp)
+    writeLines(as.character(x[2]), con=fp)
+  })
   close(fp)
 }
