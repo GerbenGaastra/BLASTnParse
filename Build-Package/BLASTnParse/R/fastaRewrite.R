e3ddfd57d15a353ee6f15f601d50ea34ae29061b
GerbenGaastra <gaastra@gtga.nl>
2012-03-19 14:32:27 +0100

    updated fastaRewrite documentation and added basis defline error checking

--- Build-Package/BLASTnParse/R/fastaRewrite.R
+++ Build-Package/BLASTnParse/R/fastaRewrite.R
@@ -7,14 +7,23 @@
 ## a vector which can be written to file as a valid (ncbi) fasta file
 ## input collumn 1 must contain the fasta ID
 ## input collumn 2 must contain the sequence
-fastaRewrite<- function(input,outFile) {
+fastaRewrite<- function(input,outFile,checkID=FALSE) {
   if( missing(input) ) { stop("argument 'input' is missing") }
   if( missing(outFile) ) { stop("argument 'outFile' is missing") }
   if( ncol(input) != 2 ) { stop("'input' has does not have 2 collumns") }
   if( mode(outFile) != "character" ) { stop("mode 'outFile' is not a char string") }
   fp <- file(outFile,"w")
   apply( input,1,function(x) {
-    writeLines(as.character(x[1]), con=fp)
+    if(checkID) {
+      if( substr(as.character(x[1]),1,1) != ">") {
+        defline <- paste(">",as.character(x[1]),collapse="",sep="")
+        writeLines(defline, con=fp)
+      } else {
+        writeLines(as.character(x[1]), con=fp)
+      }
+    } else {
+      writeLines(as.character(x[1]), con=fp)
+    }
     writeLines(as.character(x[2]), con=fp)
   })
   close(fp)
